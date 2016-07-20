906125b81b240a81984fe0e88a7a42ea7974f340
Joshua Mabry <inkandcapture@gmail.com>
2016-07-19 23:51:43 -0400

    Successfully pulled in and displayed data from Petfinder API

--- public/js/controllers/adoptCtrl.js
+++ public/js/controllers/adoptCtrl.js
@@ -6,12 +6,24 @@ angular.module('adoptCtrl', []).controller('adoptController', function($scope) {
 
 			function displayDogs (data) {
 				var dogHTML = '<div class="row section>';
-				$.each(data.pets, function(i, dog){
+				$.each(data.petfinder.pets.pet, function (i, dog){
 					dogHTML += '<div class="col-sm-3 col-md-3">';
 					dogHTML += '<div class="thumbnail">';
+					dogHTML += '<img src=" ' + dog.media.photos.photo + ' " alt="...">';
+					dogHTML += '<div class="caption">';
+					dogHTML += '<h2>' + dog.name.$t + '</h2>';
+					dogHTML += '<h4>' + dog.breeds.breed + '</h4>';
+					dogHTML += '<h4>'+ dog.age.$t +''+ dog.sex.$t +''+ dog.size.$t +'</h4>';
+					dogHTML += '<p>' + dog.description.$t + '</p>'
+					dogHTML += '<a href="#" class="btn btn-primary" role="button">More About Larry</a>';
+					dogHTML += '</div>';
+					dogHTML += '</div>';					
+					dogHTML += '</div>';
 				});	
+					dogHTML += '</div>';
+					$('#dogs').html(dogHTML);
 
-			  console.log( "success" );
+/*			  console.log( "success" );
 			  })
 			  .done(function() {
 			    console.log( "second success" );
@@ -24,7 +36,7 @@ angular.module('adoptCtrl', []).controller('adoptController', function($scope) {
 			  });
 
 			jqxhr.complete(function() {
-			  console.log( "second complete" );
+			  console.log( "second complete" );*/
 			});
 
 /*	$.getJSON( 'http://api.petfinder.com/shelter.getPets?key=a86e7c607977efb4defb7da363c0a657&id=KY314&format=json&callback=?', 'logAPI' );
